f514eb2775f5b65e378528a6ab11916bd8cd3bc8
023PrashantSharma <44042702+023PrashantSharma@users.noreply.github.com>
2022-08-18 19:29:02 +0530

    cahnges in routing

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify"
 import awsConfig from "./aws-exports"
+import { BrowserRouter } from 'react-router-dom';
 Amplify.configure(awsConfig)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
